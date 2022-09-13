e3733c24117553bc9d5d0b312b7929224093a784
JessicaJoya <jessica.joyaq@gmail.com>
2022-09-13 17:39:36 -0500

    spring boot project

---
+++ src/main/java/com/Mindtic/demoJess/mindtic/DemoJessMindticApplication.java
@@ -0,0 +1,13 @@
+package com.Mindtic.demoJess.mindtic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJessMindticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJessMindticApplication.class, args);
+	}
+
+}
